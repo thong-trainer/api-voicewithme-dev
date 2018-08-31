9cdb6ec04ca52b25dd589aed6c428711ea3f36da
leykamthong <leykamthong@gmail.com>
2018-08-31 16:11:12 +0700

    New

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # api-voicewithme-dev
+
+An api was develop by Mongkot Innovation Group.
+
+## Requirements
+
+* `Create an account Google Cloud Console` to use Google Services. [Register](https://cloud.google.com)
+* `Enable Cloud Translation API` to use Translation Service.
+* `Enable Cloud Speech API` to use Speech-to-Text Service.
+
+## Getting Started
+
+Start to download or clone the [source code](https://github.com/leykamthong/api-voicewithme-dev.git) on Github.
+
+```javascript
+git clone https://github.com/leykamthong/api-voicewithme-dev.git
+```
+
+Go to Translation API Client Libraries. [Document](https://cloud.google.com/translate/docs/reference/libraries#client-libraries-install-nodejs)
+
+
+Go to Speech-to-Text Client Libraries. [Document](https://cloud.google.com/speech-to-text/docs/reference/libraries#client-libraries-install-nodejs)
+
+Login to `gcloud`
+```javascript
+gcloud auth login
+```
--- index.js
+++ index.js
@@ -1,13 +1,250 @@
 // express
 const express = require('express');
 const app = express();
+// Add headers
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:23477');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// configure the app to use bodyParser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// database
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/voicewithme-dev", function(error) {
+  if (error)
+    console.log(error);
+  console.log("connection successful");
+});
+mongoose.Promise = global.Promise;
+// middleware
+const logger = require('morgan');
+app.use(logger('dev'))
+// public folder
+app.use('/public', express.static('./public'));
+// webiste
+app.use(express.static('public'));
+// models
+const User = require('./models/user');
+const Parent = require('./models/parent');
+const Item = require('./models/item');
+const Feedback = require('./models/feedback');
+const Contact = require('./models/contact');
+// create file or folder
+const fs = require('fs');
+// upload file
+const multer = require('multer');
+const path = require('path');
+
+// ==========================================================================
+// GOOGLE CLOUD API =========================================================
+// ==========================================================================
+const Translate = require('@google-cloud/translate');
+const speech = require('@google-cloud/speech');
+const projectId = 'aa6486b9a97774c94284daa0839a97d0db1808a2';
+const translate = new Translate({projectId: projectId});
+const clientSpeechToText = new speech.SpeechClient();
+
+// ==========================================================================
+// UPLOAD FUNCTIONS =========================================================
+// ==========================================================================
+const storage = multer.diskStorage({
+  destination: function(req, file, next) {
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    var folder = './public/uploads/' + year;
+    // if the folder not exist yet, then create
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+    folder += '/' + month;
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+
+    next(null, folder)
+  },
+  filename: function(req, file, next) {
+    next(null, Date.now() + path.extname(file.originalname));
+  }
+});
+// check file type
+function checkFileType(file, next) {
+  // allowed extension
+  const filetypes = /jpeg|jpg|png|gif|wav/;
+  // check extension
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // check mime type
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return next(null, true);
+  } else {
+    next('Error: Alowed Images Only!');
+  }
+}
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 100000000
+  },
+  fileFilter: function(req, file, next) {
+    checkFileType(file, next);
+  }
+}).single('image');
+
+// ==========================================================================
+// API FUNCTIONS ============================================================
+// ==========================================================================
 app.get('/', function(req, res, next) {
   res.send("Welcome to VoiceWIthMe API");
 });
 
+app.post('/api/speech-to-text', function(req, res, next) {
+  // The target language (eg: en, km, ...)
+  const target = req.query.target;
+  const platform = req.query.platform;
+
+  if (target == undefined || platform == undefined) {
+    res.status(500).json({message: "Bad Query", success: false});
+    return;
+  }
+
+  upload(req, res, function(err) {
+    console.log(req.file);
+    if (err) {
+      console.log("Photo API ERROR: " + err);
+      res.send("Error");
+    } else {
+      if (req.file == undefined) {
+        res.send("Error: No file");
+      } else {
+        // uploaded successful
+        const filename = req.file.path;
+        const encoding = 'LINEAR16';
+        var sampleRateHertz = 16000;
+        const languageCode = target;
+        const config = {
+          encoding: encoding,
+          sampleRateHertz: sampleRateHertz,
+          languageCode: languageCode
+        };
+        const audio = {
+          content: fs.readFileSync(filename).toString('base64')
+        };
+        const request = {
+          config: config,
+          audio: audio
+        };
+        // Detects speech in the audio file
+        clientSpeechToText.recognize(request).then(data => {
+          const response = data[0];
+          console.log(response);
+          const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
+          console.log(`Transcription: `, transcription);
+          res.send({"inputText": transcription, "translatedText": null, "targetTranslation": target, "inputSpeechUrl": req.file.path});
+        }).catch(err => {
+          console.error('ERROR:', err);
+        })
+      }
+    }
+  });
+});
+
+app.post('/api/translation', function(req, res, next) {
+  // The target language (eg: en, km, ...)
+  const target = req.query.target;
+  console.log(target);
+  if (target == undefined) {
+    res.status(500).json({message: "Bad Query", success: false});
+    return;
+  }
+
+  var text = req.body.inputText;
+  translate.translate(text, target).then(results => {
+    const translation = results[0];
+    console.log(`Text: ${text}`);
+    console.log(`Translation: ${translation}`);
+    res.send({"inputText": text, "translatedText": translation, "targetTranslation": req.body.targetTranslation, "inputSpeechUrl": req.body.inputSpeechUrl});
+
+  }).catch(err => {
+    console.error('ERROR:', err);
+    res.send("Translation Error");
+  });
+});
+
+// insert a new user
+app.post('/api/user', async function(req, res, next) {
+  var user = User(req.body);
+  user._id = mongoose.Types.ObjectId();
+  var result = await user.save();
+  res.send(result);
+});
+
+// insert a new parent
+app.post('/api/parent', async function(req, res, next) {
+  var parent = Parent(req.body);
+  parent._id = mongoose.Types.ObjectId();
+  var result = await parent.save();
+  res.send(result);
+});
+
+// get parents by user id
+app.get('/api/parent/:userId', async function(req, res, next) {
+  var parents = await Parent.find({userId: req.params.userId}).sort({createdAt: -1}).limit(30);
+  res.send(parents);
+});
+
+// insert a new item
+app.post('/api/item', async function(req, res, next) {
+  var item = Item(req.body);
+  item._id = mongoose.Types.ObjectId();
+  var result = await item.save();
+  res.send(result);
+});
+
+// get items by parent id
+app.get('/api/item/:parentId', async function(req, res, next) {
+  var parents = await Item.find({parentId: req.params.parentId});
+  res.send(parents);
+});
+
+// ==========================================================================
+// MIDDLEWARE ===============================================================
+// ==========================================================================
+// catch 404 errors and forward them to error handling middleware
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+// error handling middleware
+app.use(function(err, req, res, next) {
+  console.log(err);
+  const error = app.get('env') === 'development'
+    ? err
+    : {};
+  const status = err.status || 500;
+  res.status(status).send({error: error.message});
+});
 // listen for requests
-app.listen(process.env.port || 8091, function() {
-  console.log('now listening on port: localhost:8091');
+app.listen(process.env.port || 8090, function() {
+  console.log('now listening on port: localhost:8090');
 });
